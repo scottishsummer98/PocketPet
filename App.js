e8caae2020c2f7bdd81c0c6b105de24457e6cdfc
Scottish Summer <samiremail98@gmail.com>
2025-01-15 15:24:52 +0600

    HomeScreen modifications

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import PetSelectionScreen from "./src/screens/PetSelectionScreen";
 import PetScreen from "./src/screens/PetScreen";
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -12,19 +13,20 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name="Home"
-          options={{ title: "PocketPet" }}
           component={HomeScreen}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="PetSelectionScreeen"
+          options={{ title: "PocketPet" }}
+        />
+        {/* <Stack.Screen
+          name="PetSelectionScreen"
           component={PetSelectionScreen}
           options={{ title: "Select a Pet" }}
-        ></Stack.Screen>
+        />
+
         <Stack.Screen
           name="PetScreen"
           component={PetScreen}
           options={{ title: "Your Pet" }}
-        ></Stack.Screen>
+        /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <div>
-      HomeScreen
-    </div>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to PocketPet!</Text>
+      <Button
+        title="Select Your Pet"
+        onPress={() => navigation.navigate("PetSelectionScreen")}
+      />
+    </View>
+  );
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: { width: 200, height: 200, marginBottom: 20 },
+  title: { fontSize: 20, fontWeight: "bold", marginBottom: 20 },
+});
+
+export default HomeScreen;
