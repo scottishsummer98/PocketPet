fa8de91e8855cc458e779e0ae2e6e870554394e6
mahmud501 <mahmudulhasanmukta@gmail.com>
2025-01-15 15:03:41 +0600

    Package install

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
   return (
